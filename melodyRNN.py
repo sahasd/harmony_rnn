3f5a4b012716179e1370e06bfdfdabb35ae79fb4
sahasd <sahasd@gmail.com>
2017-10-03 10:04:39 -0700

    added dictionary genrator

--- melodyRNN.py
+++ melodyRNN.py
@@ -1,7 +1,15 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.contrib import rnn
 import random
 import collections
 import time
 
+
+#generate dictionaries to represent characters numerically and vice versa
+def build_dataset(file):
+	data = open(file, 'r').read()
+	unique_notes = list(set(data))
+	dictionary = dict()
+	for i in range(len(unique_notes)):
+		dictionary[unique_notes[i]] = i
+	rev_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+	return dictionary,rev_dictionary
