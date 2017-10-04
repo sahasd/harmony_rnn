e59bd6dd52c37a42c2c51a7dc0e780cf7a67bb33
sahasd <sahasd@gmail.com>
2017-10-03 17:13:12 -0700

    curated massive training set

--- melodyRNN.py
+++ melodyRNN.py
@@ -1,8 +1,12 @@
+import tensorflow
+from tensorflow.contrib import rnn
 import numpy as np
 import random
-import collections
-import time
 
+#training paramaters
+learning_rate = 0.001
+epochs = 10000
+display_step = 200
 
 #generate dictionaries to represent characters numerically and vice versa
 def build_dataset(file):
@@ -13,3 +17,18 @@ def build_dataset(file):
 		dictionary[unique_notes[i]] = i
 	rev_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
 	return dictionary,rev_dictionary
+
+dictionary, rev_dictionary = build_dataset('sample_training.txt')
+num_notes = len(dictionary)
+
+#hyperparamaters
+num_input = num_notes # 1 hot vector representing music note
+timesteps = num_notes
+hidden_cells = 128
+num_classes = num_notes 
+
+X = tf.placeholder('float', [None, timesteps, num_input])
+Y = tf.placeholder('float', [None, num_classes]) 
+
+
+
